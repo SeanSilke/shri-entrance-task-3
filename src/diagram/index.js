545efe553a59fcb13a07b444ef8429ef5c73f090
Sergey Orlov <OrlovMail@gmail.com>
2018-01-05 03:06:30 +0300

    Removed main component from diagram

--- src/diagram/index.js
+++ src/diagram/index.js
@@ -1,10 +1,7 @@
 import React from 'react'
 import './style.css'
 import {List} from './list'
-import {Main} from './main'
 
 export const Diagram = () => <div className='App-diagram'>
   <List></List>
-  <Main></Main>
-
 </div>
--- src/diagram/main/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import './style.css'
-
-export const Main = () => <div className='diagram-main'>
-  <div></div>
-</div>
