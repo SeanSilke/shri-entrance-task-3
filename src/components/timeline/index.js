7360ff76560d45ebc75103620be49117b63b5a71
Sergey Orlov <OrlovMail@gmail.com>
2018-01-14 04:16:17 +0300

    Add curr time line

--- src/components/timeline/index.js
+++ src/components/timeline/index.js
@@ -3,9 +3,15 @@ import './style.css'
 import {HOURS_RANGE} from '../../data/constants'
 
 export const Timeline = () => <div className='App-timeline'>
+  <div className='App-timeline-currtime'>
+      11:05
+      <div className='App-timeline-currtime-line'>
+
+      </div>
+  </div>
   {HOURS_RANGE.map(id =>
-    <div className='App-timeline-time' key={id}>
+    <div className='App-timeline-hour' key={id}>
       {id}
-      <div className='App-timeline-time-line'> </div>
+      <div className='App-timeline-hour-line'> </div>
     </div>)}
 </div>
