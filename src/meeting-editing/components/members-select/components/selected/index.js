fb29e98541c2ee080bf53fa38dd803386e147590
Sergey Orlov <OrlovMail@gmail.com>
2018-01-17 23:51:51 +0300

    Added selected participants area

---
+++ src/meeting-editing/components/members-select/components/selected/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './style.css'
+
+const data = [
+  'Лекс Лютер', 'ТА', 'ДР',
+  'Лекс Лютер', 'Томас Андерсон', 'Дарт Вейдер',
+  'Лекс Лютер', 'Томас Андерсон', 'Дарт Вейдер',
+  'Лекс', 'Томас Андерсон', 'Дарт Вейдер',
+]
+
+const Item = ({title}) =>   <div className="multi-select-item">
+    <div className='multi-select-userpic'></div>
+    <span>{title} </span>
+    <div className="multi-select-remove"></div>
+  </div>
+
+export const Selected = () => <div className='multi-select-selected'>
+  {data.map((name)=><Item title={name}/>)}
+
+</div>
--- src/meeting-editing/components/members-select/index.js
+++ src/meeting-editing/components/members-select/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Dropdown} from './components/dropdown'
+import {Selected} from './components/selected'
 import './style.css'
 
 
@@ -7,5 +8,6 @@ export const MembersSelect = () =>  <div className='multi-select'>
   <div className='editing-input-title'>Участники</div>
   <input className='editing-input' placeholder='Например, Тор Одинович'></input>
   <Dropdown/>
-  
+  <Selected/>
+
 </div>
