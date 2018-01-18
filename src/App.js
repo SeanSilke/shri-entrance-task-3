49b6074b8bb3ced2014b6edf9536f751791a12af
Sergey Orlov <OrlovMail@gmail.com>
2018-01-18 21:59:29 +0300

    Begin to work on mobile view. Among others write css for header, timeline and diagram list.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import {Diagram} from './diagram'
-import {Editing} from './meeting-editing';
+// import {Editing} from './meeting-editing';
 import './app.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <Diagram/> */}
-        <Editing/>
+        <Diagram/>
+        {/* <Editing/> */}
       </div>
     );
   }
