a31b72397d02bc480faea0f6e9b623f7891d1533
Sergey Orlov <OrlovMail@gmail.com>
2018-01-21 06:58:15 +0300

    Add react-router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom'
 import {Diagram} from './diagram'
-// import {Editing} from './meeting-editing';
+import {Editing} from './meeting-editing';
 import './app.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Diagram/>
-        {/* <Editing/> */}
-      </div>
+        <Router>
+          <div
+          className="App">
+            <Route path="/diagram" component={Diagram}/>
+            <Route path="/editing" component={Editing}/>
+          </div>
+        </Router>
     );
   }
 }
