e0c7ddf907dc446b1938b325b01d8969cf0cd84a
Sergey Orlov <OrlovMail@gmail.com>
2018-01-22 22:44:51 +0300

    Added mibile style for timepicker

--- src/meeting-editing/components/timepicker/index.js
+++ src/meeting-editing/components/timepicker/index.js
@@ -2,22 +2,24 @@ import React from 'react'
 import './style.css'
 
 export const Timepicker = () => <div className='time'>
-    <div>
+
+    <div className='time-input-date'>
       <div className='editing-input-title'> Дата </div>
-      <input className='editing-input time-input-date' value='14 декабря, 2017' />
+      <input className='editing-input' value='14 декабря, 2017' />
     </div>
 
-    <div>
+    <div className='time-input-time'>
       <div className='editing-input-title'>
         Начало
       </div>
-      <input className='editing-input time-input-time' value='16:00' />
+      <input className='editing-input' value='16:00' />
     </div>
-    <div className='time-dash'>
-      —
-    </div>
-    <div>
+
+    <div className='time-dash'> — </div>
+
+    <div className='time-input-time'>
       <div className='editing-input-title'> Конец </div>
-      <input className='editing-input time-input-time' value='16:30' />
+      <input className='editing-input' value='16:30' />
     </div>
+
   </div>
