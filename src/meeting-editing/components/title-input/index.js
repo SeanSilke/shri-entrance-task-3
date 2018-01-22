9874f3800a37b2259415dabedc5817238486c20a
Sergey Orlov <OrlovMail@gmail.com>
2018-01-22 22:44:11 +0300

    Add mobile style for title-input

--- src/meeting-editing/components/title-input/index.js
+++ src/meeting-editing/components/title-input/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import './style.css'
 
-export const TitleInput = () => <div className='input'>
+export const TitleInput = () => <div className='title-input'>
     <div className='editing-input-title'> Тема </div>
     <input className='editing-input' value='О чём будете говорить?' onChange={()=>console.log('hi')} />
   </div>
