2c88db44385131d860e14199490d29ba127a4062
Sergey Orlov <OrlovMail@gmail.com>
2018-01-30 11:49:37 +0300

    Add custom path to button. And fix style

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
 
-export const Button = ({ title }) => (
-  <Link to="/new" className="App-button">
+export const Button = ({ title, to }) => (
+  <Link to={to} className="App-button">
     {title}
   </Link>
 );
